9f20d5e6cf1463c6b429d4ad839990b13aecbc7c
Adam Cox <mr.adamcox@gmail.com>
2020-09-12 08:47:27 -0500

    update api key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UHiOfqLCCfbyj-zfyqajQOujic67P0qJaGyRE-BZM1Y/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCw67BzZKeoTegjF5bh69ibpQmEqHCRonw';
+var googleApiKey = 'AIzaSyCaIQNVW4SyS8-XW34E2AicZtOpsPd1OF8';
