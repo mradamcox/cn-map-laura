c8607c36c6245078173c009aac1d965263aea658
Adam Cox <mr.adamcox@gmail.com>
2020-09-12 10:28:02 -0500

    change point sheet name

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,7 +2,7 @@ var constants = {
   // Tabs in Google Sheet
   notesSheetName: 'Notes',
 	optionsSheetName: 'Options',
-	pointsSheetName: 'Points',
+	pointsSheetName: 'Ticket List',
   polygonsSheetName: 'Polygons',
   polylinesSheetName: 'Polylines',
   /* OPTIONS */
