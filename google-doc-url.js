9822984d51705e823a392933bbbb6bb8db39f08d
Adam Cox <mr.adamcox@gmail.com>
2020-09-12 08:34:22 -0500

    add google sheet url and api key

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UHiOfqLCCfbyj-zfyqajQOujic67P0qJaGyRE-BZM1Y/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyCw67BzZKeoTegjF5bh69ibpQmEqHCRonw';
